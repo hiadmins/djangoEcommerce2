aeea396939e240efe633c33c9a3316923294b5b8
Your Name <fbi32003@yahoo.com>
2024-12-08 09:18:56 +0330

    the settings file configuration is updated.

--- djangoEcommerce2/djangoEcommerce2/settings.py
+++ djangoEcommerce2/djangoEcommerce2/settings.py
@@ -11,7 +11,10 @@ https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
 from pathlib import Path
+import os
+from dotenv import load_dotenv
 
+load_dotenv()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-q-c&gtv@k!_cloda77$7+jdvg01d%((2k5j)6f0%a&m=!z3ljt'
+SECRET_KEY = os.environ.get('SECURITY_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
